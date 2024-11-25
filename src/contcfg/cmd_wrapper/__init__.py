db8aecd98c2cfc464c443ea98b144aa1ce55ab68
DeEMO <yzzxrx@foxmail.com>
2024-11-25 21:42:59 +0800

    :sparkles: Update versioning strategy and disable test step in CI workflow

--- src/contcfg/cmd_wrapper/__init__.py
+++ src/contcfg/cmd_wrapper/__init__.py
@@ -1,6 +1,8 @@
 from .tccmd_wrapper import TCCmdWrapper, DockerCmdWrapper
 from .exception import RateValueError, ContainerNotFoundError
 
+__version__ = "0.0.2"
+
 __all__ = [
     "TCCmdWrapper",
     "DockerCmdWrapper",
