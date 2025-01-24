ca11910c25008e1148a46e0fa0a702cdcb1bfc73
DeEMO <yzzxrx@foxmail.com>
2025-01-24 12:26:17 +0800

    :bump: Bump version to 0.0.5

--- src/contcfg/__init__.py
+++ src/contcfg/__init__.py
@@ -1,7 +1,7 @@
 from .cmd_wrapper import TCCmdWrapper, DockerCmdWrapper
 from .container_net_ctrl import ConNetController, ConNetServer
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 __all__ = [
     "TCCmdWrapper",
